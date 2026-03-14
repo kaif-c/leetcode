29460b0d6231e05ed039e93cdb5950cb7a46db77
Kaif Chowdhury <261884279+kaif-c@users.noreply.github.com>
2026-03-14 16:42:30 +0600

    TwoSome Solution

---
+++ twosome/notes.md
@@ -0,0 +1,18 @@
+### Name: Two Some
+
+## Difficulty: Easy
+
+**Description**:
+Return 2 numbers that result in target
+
+**Constraints**:
+
+- `2 <= nums.length <= 104`
+- `-109 <= nums[i] <= 109`
+- `-109 <= target <= 109`
+- Only one valid answer exists
+
+**Idea**: 
+
+For each element loop through the entire array? `O(n^2)` complexity
+How can i get `< O(n^2)`? Fo every element 
---
+++ twosome/twosum.cpp
@@ -0,0 +1,29 @@
+#include <vector>
+#include <unordered_map>
+using namespace std;
+
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        // Map of every element (key) to its index (value) 
+        unordered_map<int, int> map;
+
+        // Create the map
+        map.reserve(nums.size());
+        for (int i = 0; i < nums.size(); ++i) // O(n)
+            map.insert({nums[i], i});
+
+        for (int i = 0; i < nums.size(); ++i) { // O(n)
+            // Find if an element exists that if added to nums[i]
+            // will result in target
+            auto it = map.find(target - nums[i]);
+            // Checking if it exists AND that there isnt a repeat
+            if (it != map.end() && it->second != i)
+                return {it->second, i};
+        }
+
+        // Error case
+        // should never happen with the constraints
+        return {};
+    }
+};
