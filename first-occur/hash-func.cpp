b35e6b5543e0c62892c09a59f188811b396d71a7
Agent11196 <13097232-Agent11196@users.noreply.gitlab.com>
2026-03-30 15:33:04 +0600

    Faster Hashing

--- first-occur/hash-func.cpp
+++ first-occur/hash-func.cpp
@@ -3,14 +3,29 @@
 #include <string_view>
 using namespace std;
 
+/**
+ * THOUGHT:
+ * Instead of O(n^2) a O(n) approach woould be better since we dont know the sizes
+ * We can use hash functions for reducing time complexity
+*/
+
 class Solution {
 public:
-    int strStr(string haystack, string needle) {
-        auto needle_hash = hash<string>{}(needle);
-        for (int i = 0; i < haystack.size(); ++i) {
+    int strStr(const string haystack, const string needle) {
+        const hash<string> str_hasher;
+        const hash<string_view> view_hasher;
+        const size_t needle_hash = str_hasher(needle);
+        // Smaller stack edge case
+        if (needle.size() > haystack.size())
+            return -1;
+        // O(n) loop
+        for (int i = 0; i < haystack.size() - needle.size() + 1; ++i) {
+            // Only check if first character matches
             if (haystack[i] == needle[0]) {
-                string_view hay_view = string_view(haystack.data() + i, needle.size());
-                if (hash<string_view>{}(hay_view) == needle_hash)
+                string_view hay_view = haystack;
+                hay_view = hay_view.substr(i, needle.size());
+                // Found match
+                if (view_hasher(hay_view) == needle_hash)
                     return i;
             }
         }
